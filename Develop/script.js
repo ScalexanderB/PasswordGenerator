c436793872f05f1318408db552de89867db6ec01
Scott Bradley <scottabradley91@gmail.com>
2021-09-10 20:12:16 -0400

    Added the variables and prompts for password customization

--- Develop/script.js
+++ Develop/script.js
@@ -23,6 +23,42 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var symbol = "!@#$%^&*()_+-={}[]:;<,>.?/'\"|\\";
 
+/* GLOBAL VARIABLES END */
+
+/* GENERATOR FUNCTIONS BEGIN */
+
+function generatePassword() {
+  var passwordLength = window.prompt("How many characters would you like your password to be? Please enter a value between 8 and 128.");
+
+    if (passwordLength <= 7 || passwordLength >= 129) {
+      window.alert("Invalid entry. Please enter a value between 8 and 128");
+    }
+    
+    else {
+      console.log("Password length is " + passwordLength);
+    }
+
+        // Ask the user if they want lowercase
+        var askLower = confirm("Would you like to include lowercase characters?");
+
+        // Ask the user if they want uppercase
+        var askUpper = confirm("Would you like to include uppercase characters?");
+
+        // Ask the user if they want numbers
+        var askNumber = confirm("Would you like to include numbers?");
+
+        // Ask the user if they want special characters
+        var askSymbol = confirm("Would you like to include special characters?");
+
+        while (askLower !== true && askUpper !== true && askNumber !== true && askSymbol !== true){
+          window.alert("You must select at least 1 parameter.")
+          var askLower = confirm("Would you like to include lowercase characters?");
+          var askUpper = confirm("Would you like to include uppercase characters?");
+          var askNumber = confirm("Would you like to include numbers?");
+          var askSymbol = confirm("Would you like to include special characters?");
+        }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
